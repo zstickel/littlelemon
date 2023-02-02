3f566c3d81465aae590af702aaca54c668eabd2f
Zane Stickel <zanemstickel@gmail.com>
2023-02-01 20:23:21 -0500

    first commit

---
+++ littlelemon/littlelemon/littlelemonApp.swift
@@ -0,0 +1,17 @@
+//
+//  littlelemonApp.swift
+//  littlelemon
+//
+//  Created by Zane Stickel on 2/1/23.
+//
+
+import SwiftUI
+
+@main
+struct littlelemonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
